121fa265022a196741b6eeb2a8c95388f31bbe51
ktsn <ktsn55@gmail.com>
2018-03-27 11:28:36 +0900

    feat: overload original helper types to support Vue 2.5 types

--- helpers.d.ts
+++ helpers.d.ts
@@ -1,32 +1,67 @@
 import Vue from 'vue'
 import 'vuex'
 
+type Accessor<T, State, Getters> = {
+  [K in keyof T]: <V extends Vue>(this: V, state: State, getters: Getters) => T[K]
+} & {
+  [key: string]: <V extends Vue>(this: V, state: State, getters: Getters) => any
+}
+
+interface ComputedMapper<T> {
+  <Key extends keyof T, Map extends Record<string, Key>>(map: Map): { [K in keyof Map]: () => T[Map[K]] }
+  <Key extends keyof T>(map: Key[]): { [K in Key]: () => T[K] }
+}
+
+interface ComputedStateMapper<State, Getters> {
+  <T>(map: Accessor<T, State, Getters>): { [K in keyof T]: () => T[K] }
+}
+
+interface MethodsMapper<T, R> {
+  <Key extends keyof T, Map extends Record<string, Key>>(map: Map): { [K in keyof Map]: (payload: T[Map[K]]) => R }
+  <Key extends keyof T>(map: Key[]): { [K in Key]: (payload: T[K]) => R }
+}
+
+interface StrictNamespacedMappers<State, Getters, Mutations, Actions> {
+  mapState: ComputedMapper<State> & ComputedStateMapper<State, Getters>
+  mapGetters: ComputedMapper<Getters>
+  mapMutations: MethodsMapper<Mutations, void>
+  mapActions: MethodsMapper<Actions, Promise<any>>
+}
+
+
 declare module 'vuex/types/helpers' {
-  type Accessor<T, State, Getters> = {
-    [K in keyof T]: <V extends Vue>(this: V, state: State, getters: Getters) => T[K]
-  } & {
-    [key: string]: <V extends Vue>(this: V, state: State, getters: Getters) => any
+  interface Mapper<R> {
+    <K extends string>(map: K[]): Record<K, R>
+    <K extends string>(map: Record<K, string>): Record<K, R>
   }
 
-  interface ComputedMapper<T> {
-    <Key extends keyof T, Map extends Dictionary<Key>>(map: Map): { [K in keyof Map]: () => T[Map[K]] }
-    <Key extends keyof T>(map: Key[]): { [K in Key]: () => T[K] }
+  interface MapperWithNamespace<R> {
+    <K extends string>(namespace: string, map: K[]): Record<K, R>
+    <K extends string>(namespace: string, map: Record<K, string>): Record<K, R>
   }
 
-  interface ComputedStateMapper<State, Getters> {
-    <T>(map: Accessor<T, State, Getters>): { [K in keyof T]: () => T[K] }
+  interface FunctionMapper<F, R> {
+    <K extends string>(map: Record<K, (this: Vue, fn: F, ...args: any[]) => any>): Record<K, R>
   }
 
-  interface MethodsMapper<T, R> {
-    <Key extends keyof T, Map extends Dictionary<Key>>(map: Map): { [K in keyof Map]: (payload: T[Map[K]]) => R }
-    <Key extends keyof T>(map: Key[]): { [K in Key]: (payload: T[K]) => R }
+  interface FunctionMapperWithNamespace<F, R> {
+    <K extends string>(
+      namespace: string,
+      map: Record<K, (this: Vue, fn: F, ...args: any[]) => any>
+    ): Record<K, R>
   }
 
-  interface StrictNamespacedMappers<State, Getters, Mutations, Actions> {
-    mapState: ComputedMapper<State> & ComputedStateMapper<State, Getters>
-    mapGetters: ComputedMapper<Getters>
-    mapMutations: MethodsMapper<Mutations, void>
-    mapActions: MethodsMapper<Actions, Promise<any>>
+  interface MapperForState {
+    <S, K extends string>(
+      map: Record<K, (this: Vue, state: S, getters: any) => any>
+    ): Record<K, Computed>
+  }
+
+  interface MapperForStateWithNamespace {
+    <S, K extends string>(
+      namespace: string,
+      map: Record<K, (this: Vue, state: S, getters: any) => any>
+    ): Record<K, Computed>
   }
 
   export function createNamespacedHelpers(namespace: string): NamespacedMappers
